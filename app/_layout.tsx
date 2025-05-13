3a674cf41fe572aa376717dcc04d5311a969aea0
rangel-borene <96886855+rangel-borene@users.noreply.github.com>
2025-05-13 15:47:41 -0300

    Menu Contas abre somente chats.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,21 +7,15 @@ export default function RootLayout() {
   const { selectedAccount } = useAccount();
   return (
     <Stack
-      // screenOptions={{
-      //   headerShown: false,
-      //   animation: 'slide_from_right', // Adicione transições suaves
-      //   contentStyle: {
-      //     backgroundColor: '#ffffff' // Cor de fundo padrão
-      //   }
-      // }}
       screenOptions={({ route }) => ({
-        headerTitle: selectedAccount,
+        headerRight: () => route.name === 'chats' ? <AccountMenu /> : null,
+        headerTitle: route.name === 'chats' ? 'Conversas' : selectedAccount,
+        headerShown: route.name === 'chats',
         headerTitleStyle: {
           fontWeight: '600',
           fontSize: 18,
           color: '#333',
         },
-        headerRight: () => <AccountMenu />,
         tabBarStyle: {
           height: 60,
           paddingBottom: 8,
@@ -29,11 +23,11 @@ export default function RootLayout() {
       })}
     >
       {/* Mantenha apenas as telas que precisam de configurações específicas */}
+
       <Stack.Screen
         name="chats"
         options={{
           title: 'Chats',
-          headerShown: true,
           headerTitle: 'Conversas',
           // headerBackTitle: 'Voltar'
         }}

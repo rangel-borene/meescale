00a28118cd07c9ac6d322065fe82d99376a79b91
rangel-borene <96886855+rangel-borene@users.noreply.github.com>
2025-05-13 13:07:02 -0300

    deleei tabas]

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,38 @@
 // app/_layout.tsx
 import { Stack } from "expo-router";
+import AccountMenu from "./components/AccountMenu";
+import { useAccount } from "./context/AccountContext";
 
 export default function RootLayout() {
+  const { selectedAccount } = useAccount();
   return (
     <Stack
-      screenOptions={{
-        headerShown: false,
-        animation: 'slide_from_right', // Adicione transições suaves
-        contentStyle: {
-          backgroundColor: '#ffffff' // Cor de fundo padrão
-        }
-      }}
+      // screenOptions={{
+      //   headerShown: false,
+      //   animation: 'slide_from_right', // Adicione transições suaves
+      //   contentStyle: {
+      //     backgroundColor: '#ffffff' // Cor de fundo padrão
+      //   }
+      // }}
+      screenOptions={({ route }) => ({
+        headerTitle: selectedAccount,
+        headerTitleStyle: {
+          fontWeight: '600',
+          fontSize: 18,
+          color: '#333',
+        },
+        headerRight: () => <AccountMenu />,
+        tabBarStyle: {
+          height: 60,
+          paddingBottom: 8,
+        },
+      })}
     >
       {/* Mantenha apenas as telas que precisam de configurações específicas */}
       <Stack.Screen
         name="chats"
         options={{
-          title: 'Chatsssss',
+          title: 'Chats',
           headerShown: true,
           headerTitle: 'Conversas',
           // headerBackTitle: 'Voltar'
